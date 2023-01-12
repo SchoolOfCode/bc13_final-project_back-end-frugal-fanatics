978b5317c7feee717d911f57b4760a7432d50326
mvhmz81 <mvhmz@yahoo.co.uk>
2023-01-12 06:18:37 +0000

    wrote all async functions and router requests for models and routes get and post functions

--- routes/expensesRoutes.js
+++ routes/expensesRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { getExpenses, createExpenses } from "../models/expensesModels.js";
 const router = express.Router();
-
-import { getExpenses } from "../models/expensesModels.js";
 
 router.get("/"),
   async function (req, res) {
-    const result = await getExpenses(req.query.user_ID);
-    console.log(`this is from week ${result}`);
+    const result = await getExpenses(req.query.userID);
+    console.log(`this is from expenses ${result}`);
+    return res.status(200).json({ success: true, payload: result });
+  };
+
+router.post("/"),
+  async function (req, res) {
+    const users = await createExpenses(req.body);
     return res.status(200).json({ success: true, payload: result });
   };
